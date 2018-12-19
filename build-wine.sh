6943869c249416187e68eff6a00b0459db48fb5e
gasinvein <gasinvein@gmail.com>
2018-12-19 19:47:10 +0300

    Add build script

---
+++ build-wine.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BRANCH=${1:-"stable"}
+REPO=$2
+EXPORT_ARGS=$3
+FB_ARGS=$4
+
+set -x
+for flavour in vanilla staging; do
+    for ARCH in x86_64 i386; do
+        flatpak-builder -v --force-clean --ccache --sandbox --delete-build-dirs \
+            --arch=${ARCH} --repo="${REPO}" --default-branch="${BRANCH}" \
+            ${FB_ARGS} ${EXPORT_ARGS} "build/wine/$flavour/$ARCH" \
+            "ua.org.brezblock.q4wine.wine.$flavour.yml"
+    done
+    flatpak build-commit-from --verbose ${EXPORT_ARGS} \
+        --src-ref="runtime/ua.org.brezblock.q4wine.wine.$flavour/i386/${BRANCH}" "${REPO}" \
+        "runtime/ua.org.brezblock.q4wine.wine.$flavour.compat32/x86_64/${BRANCH}"
+done
