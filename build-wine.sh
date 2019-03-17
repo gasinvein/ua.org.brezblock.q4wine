51a8920d727941b5b3f751f03efbe4d01a194e95
gasinvein <gasinvein@gmail.com>
2019-03-17 17:35:22 +0300

    Use options in builder script

--- build-wine.sh
+++ build-wine.sh
@@ -1,20 +1,56 @@
 #!/bin/bash
+set -u -e
 
-BRANCH=${1:-"stable"}
-REPO=$2
-EXPORT_ARGS=$3
-FB_ARGS=$4
-FLAVOURS=${@:5}
+FLAVOURS=()
 
-set -e -x
-for flavour in $FLAVOURS; do
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -b|--branch)
+            BRANCH="$2"
+            shift
+            shift
+        ;;
+        -r|--repo)
+            REPO="$2"
+            shift
+            shift
+        ;;
+        -a|--builder-args)
+            FB_ARGS="$2"
+            shift
+            shift
+        ;;
+        -e|--export-args)
+            EXPORT_ARGS="$2"
+            shift
+            shift
+        ;;
+        -d|--build-dir)
+            BUILD_DIR="$2"
+            shift
+            shift
+        ;;
+        *)
+            FLAVOURS+=("$1")
+            shift
+        ;;
+    esac
+done
+
+[ -v "BUILD_DIR" ] || BUILD_DIR="./build"
+
+for flavour in "${FLAVOURS[@]}"; do
     for ARCH in x86_64 i386; do
+        set -x
         flatpak-builder --force-clean --ccache --sandbox --delete-build-dirs \
             --arch=${ARCH} --repo="${REPO}" --default-branch="${BRANCH}" \
-            ${FB_ARGS} ${EXPORT_ARGS} "build/wine/$flavour/$ARCH" \
+            ${FB_ARGS} ${EXPORT_ARGS} "${BUILD_DIR}/wine/$flavour/$ARCH" \
             "ua.org.brezblock.q4wine.wine.$flavour.yml"
+        set +x
     done
+    set -x
     flatpak build-commit-from --verbose ${EXPORT_ARGS} \
         --src-ref="runtime/ua.org.brezblock.q4wine.wine.$flavour/i386/${BRANCH}" "${REPO}" \
         "runtime/ua.org.brezblock.q4wine.wine.$flavour.compat32/x86_64/${BRANCH}"
+    set +x
 done
